c764ced45870db84eff0f218439aae952a64e082
Oleksandr <lysakova@yahoo.com>
2024-09-29 07:37:51 +0200

    fix: rename to correct case

--- src/entities/books/bookRoutes.ts
+++ src/entities/books/bookRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { BookController } from './BookControllerTemp';
+import { BookController } from './BookController';
 import { asyncErrorCatch } from '../../utils/asyncErrorCatch';
-import { BookService } from './BookServiceTemp';
+import { BookService } from './BookService';
 import { Book } from './Book';
 
 export const bookRouter = Router();

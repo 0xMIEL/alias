b4961f5b8a4b6ceb4800af6ec9729b9bc6632aff
Oleksandr <lysakova@yahoo.com>
2024-10-01 08:17:11 +0200

    feat: update database schema

--- src/entities/gameRooms/GameRoom.ts
+++ src/entities/gameRooms/GameRoom.ts
@@ -1,19 +1,57 @@
 import mongoose, { Schema, model } from 'mongoose';
 import { gameRoomStatuses, IGameRoom } from './types/gameRoom';
 
-const gameRoomeSchema = new Schema<IGameRoom>({
-  currentRound: { required: true, type: Number },
-  hostUserId: { required: true, type: String },
-  players: [{ team: Number, userId: mongoose.Types.ObjectId }],
-  roundsTotal: { required: true, type: Number },
-  scores: [{ score: Number, team: Number }],
-  status: {
-    enum: Object.values(gameRoomStatuses),
-    required: true,
-    type: String,
+const MIN_GAME_ROUNDS = 1;
+const MAX_GAME_ROUNDS = 10;
+const MIN_TEAM_SIZE = 2;
+const MAX_TEAM_SIZE = 4;
+const MIN_TIME_PER_ROUND_MINUTES = 1;
+const MAX_TIME_PER_ROUND_MINUTES = 5;
+
+const gameRoomeSchema = new Schema<IGameRoom>(
+  {
+    currentRound: { required: true, type: Number },
+    hostUserId: { required: true, type: String },
+    players: [
+      {
+        ref: 'User',
+        team: Number,
+        userId: mongoose.Types.ObjectId,
+      },
+    ],
+    roundsTotal: {
+      max: MAX_GAME_ROUNDS,
+      min: MIN_GAME_ROUNDS,
+      required: true,
+      type: Number,
+    },
+    scores: [
+      {
+        default: 0,
+        score: Number,
+        team: Number,
+      },
+    ],
+    status: {
+      default: gameRoomStatuses.lobby,
+      enum: Object.values(gameRoomStatuses),
+      required: true,
+      type: String,
+    },
+    teamSize: {
+      max: MAX_TEAM_SIZE,
+      min: MIN_TEAM_SIZE,
+      required: true,
+      type: Number,
+    },
+    timePerRound: {
+      max: MAX_TIME_PER_ROUND_MINUTES,
+      min: MIN_TIME_PER_ROUND_MINUTES,
+      required: true,
+      type: Number,
+    },
   },
-  teamSize: { required: true, type: Number },
-  timePerRound: { required: true, type: Number },
-});
+  { timestamps: true },
+);
 
 export const GameRoom = model<IGameRoom>('GameRoom', gameRoomeSchema);

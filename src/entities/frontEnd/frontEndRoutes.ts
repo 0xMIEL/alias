177295f3d482d715ecc45022cf69ef77dd91ac67
Oleksandr <lysakova@yahoo.com>
2024-10-02 15:15:52 +0200

    feat: add frontend controller and base router

---
+++ src/entities/frontEnd/frontEndRoutes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { FrontEndController } from './FrontEndController';
+import { GameRoomService } from '../gameRooms/GameRoomService';
+import { GameRoom } from '../gameRooms/GameRoom';
+
+export const fronEndRouter = Router();
+
+const gameRoomService = new GameRoomService(GameRoom);
+const frontEndController = new FrontEndController(gameRoomService);
+
+fronEndRouter
+  .route('/')
+  .get(frontEndController.getHome.bind(frontEndController));
+
+fronEndRouter
+  .route('/game-lobby')
+  .get(frontEndController.getGameLobby.bind(frontEndController));

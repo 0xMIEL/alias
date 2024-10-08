866ac6c698f02101a58855e98cdf06ff7041286e
olga-kacala <olga.kacala@gmail.com>
2024-10-08 14:09:57 +0200

    feat: logout

--- src/public/js/handleOptionsUserProfile.js
+++ src/public/js/handleOptionsUserProfile.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const optionsModal = document.getElementById('optionsModal');
     const openOptionsButton = document.getElementById('openOptionsBtn');
     const closeButton = optionsModal.querySelector('.close');
+    const logoutButton = document.getElementById('logoutButton');
 
     openOptionsButton.addEventListener('click', function () {
         optionsModal.style.display = 'block';
@@ -16,4 +17,25 @@ document.addEventListener('DOMContentLoaded', function () {
             optionsModal.style.display = 'none';
         }
     });
+  
+  if (logoutButton) {
+    logoutButton.addEventListener('click', function () {
+        fetch('/api/v1/users/logout', {
+            method: 'DELETE',
+            credentials: 'include' 
+        })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = '/'; 
+            } else {
+                alert('Logout failed. Please try again.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again.');
+        });
+    });
+}
 });
+

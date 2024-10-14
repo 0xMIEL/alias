11e040bafa40c639d7f7c577638cb2f7e47f7ccb
Rolleksy <aleksanderpurcha@gmail.com>
2024-10-14 20:47:11 +0200

    add difficulty to `getRandomWord` and add CRUD to Word

--- src/entities/word/Word.ts
+++ src/entities/word/Word.ts
@@ -2,6 +2,8 @@ import { Schema, model } from 'mongoose';
 import { IWord } from './types/word';
 
 const wordSchema = new Schema<IWord>({
+  category: { required: true, type: String },
+  difficulty: { required: true, type: String },
   value: { required: true, type: String },
 });
 
